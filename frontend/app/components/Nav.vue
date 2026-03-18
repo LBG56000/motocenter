63869595600c356ae567174ffd467412fcde2543
mini_bzh <mini.h.13.9.14.9@gmail.com>
2026-03-18 12:28:27 +0200

    light and dark mode

--- frontend/app/components/Nav.vue
+++ frontend/app/components/Nav.vue
@@ -1,22 +1,94 @@
-<script setup></script>
+<script setup>
+import ToggleSwitch from './ToggleSwitch.vue'
+const isOpen = ref(false)
+const mode = ref(false)
+const colorMode = useColorMode()
+
+function toggle_open() {
+  isOpen.value = !isOpen.value
+}
+
+colorMode.preference = computed(() => (mode.value ? 'dark' : 'light'))
+</script>
 
 <template>
-  <div class="navbar">
-    <UButton size="md" color="primary" class="logo">Motocenter</UButton>
-
-    <div class="list">
-      <UButton size="md" color="neutral" variant="ghost">Comparateur</UButton>
-      <UButton size="md" color="neutral" variant="ghost">Forum</UButton>
-      <UButton size="md" color="neutral" variant="ghost">Balades</UButton>
-      <UButton size="md" color="neutral" variant="ghost"
+  <div id="navbar-pc">
+    <div class="navbar">
+      <div class="list">
+        <UButton size="md" color="primary" class="logo">Motocenter</UButton>
+        <ToggleSwitch v-model="mode" />
+      </div>
+      <div class="list">
+        <UButton size="md" color="neutral" variant="ghost">Comparateur</UButton>
+        <UButton size="md" color="neutral" variant="ghost">Forum</UButton>
+        <UButton size="md" color="neutral" variant="ghost">Balades</UButton>
+        <UButton size="md" color="neutral" variant="ghost"
+          >Nous connaitre</UButton
+        >
+        <UButton
+          trailing-icon="i-lucide-arrow-right"
+          size="md"
+          color="neutral"
+          class="rounded-full"
+          >Connexion</UButton
+        >
+      </div>
+    </div>
+  </div>
+  <div id="navbar-mobile">
+    <div class="navbar">
+      <div class="list">
+        <UButton size="md" color="primary" class="logo">Motocenter</UButton>
+        <ToggleSwitch v-model="mode" />
+      </div>
+      <UIcon
+        :name="isOpen ? 'i-lucide-chevron-down' : 'i-lucide-menu'"
+        class="size-10"
+        @click="toggle_open"
+      />
+    </div>
+    <div v-if="isOpen" class="list-mobile">
+      <UButton
+        size="md"
+        color="neutral"
+        variant="ghost"
+        style="justify-content: center"
+        >Comparateur</UButton
+      >
+      <UButton
+        size="md"
+        color="neutral"
+        variant="ghost"
+        style="justify-content: center"
+        >Forum</UButton
+      >
+      <UButton
+        size="md"
+        color="neutral"
+        variant="ghost"
+        style="justify-content: center"
+        >Balades</UButton
+      >
+      <UButton
+        size="md"
+        color="neutral"
+        variant="ghost"
+        style="justify-content: center"
         >Nous connaitre</UButton
       >
       <UButton
-        trailing-icon="i-lucide-arrow-right"
         size="md"
         color="neutral"
-        class="connexion"
+        variant="ghost"
+        style="justify-content: center"
         >Connexion</UButton
+      >
+      <UButton
+        size="md"
+        color="neutral"
+        variant="ghost"
+        style="justify-content: center"
+        >Mon profil</UButton
       >
     </div>
   </div>
@@ -27,7 +99,7 @@
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: white;
+  background-color: var(--background);
 
   padding: 10px;
 
@@ -35,31 +107,44 @@
   top: 0;
 }
 
+.logo {
+  font-size: 1rem;
+  font-weight: bold;
+
+  padding: 10px 25px;
+
+  color: white;
+}
+
+/** Style version PC */
+@media (max-width: 1024px) {
+  #navbar-pc {
+    display: none;
+  }
+}
+
 .list {
   display: flex;
   flex-direction: row;
+  align-items: center;
   gap: 40px;
 }
 
-p {
-  align-self: center;
-  font-family: 'Popins', sans-serif;
+/** Style version mobile */
+
+@media (min-width: 1024px) {
+  #navbar-mobile {
+    display: none;
+  }
 }
 
-.logo {
-  font-size: 1.2rem;
-  font-weight: bold;
+.list-mobile {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
-  padding: 20px 50px;
-}
+  text-align: center;
 
-.connexion {
-  font-size: 1rem;
-  font-weight: 600;
-
-  border-radius: 40px;
-
-  padding: 20px 50px;
-  margin: 0;
+  background-color: white;
 }
 </style>
