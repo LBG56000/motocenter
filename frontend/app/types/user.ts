27c526355589de8903fc7710984e913cdf98da38
CApiTaiNn <morgantsloan@gmail.com>
2026-03-22 22:58:51 +0100

    feat: add interfaces for brand, messages, motorcycle, and user; remove outdated users interface

---
+++ frontend/app/types/user.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: string
+  firstname: string
+  lastname: string
+  pseudo: string
+  email: string
+  isAdmin: boolean
+  password: string
+  ridingStartYear?: number
+  createdAt: Date
+  userType: 'beginner' | 'confirmed' | 'expert' | 'other'
+  idMoto: string
+}
